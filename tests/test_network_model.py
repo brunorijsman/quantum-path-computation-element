0d208a87f542f97cbbd441a8f9ebaf47c79bd4ed
Bruno Rijsman <brunorijsman@gmail.com>
2019-07-09 14:52:42 +0200

    Remove intentional test failure

--- tests/test_network_model.py
+++ tests/test_network_model.py
@@ -5,4 +5,3 @@ import network_model
 def test_valid_network_file():
     """Test reading a valid network YAML file."""
     _model = network_model.read_network_model_from_file("tests/network-valid.yaml")
-    assert False
