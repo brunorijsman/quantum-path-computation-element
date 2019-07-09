391659f4eb9357140babfd97ad1e87224f0c07eb
Bruno Rijsman <brunorijsman@gmail.com>
2019-07-09 16:40:42 +0200

    Add code coverage minimum to pre-commit-check

--- tests/test_network_model.py
+++ tests/test_network_model.py
@@ -1,7 +1,14 @@
 """Unit tests for module network_model."""
 
+import pytest
+
 import network_model
 
 def test_valid_network_file():
     """Test reading a valid network YAML file."""
     _model = network_model.read_network_model_from_file("tests/network-valid.yaml")
+
+def test_non_existent_network_file():
+    """Test reading a network YAML file that does not exist."""
+    with pytest.raises(network_model.ReadNetworkModelError):
+        _model = network_model.read_network_model_from_file("tests/non-existent-file.yaml")
