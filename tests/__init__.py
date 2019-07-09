391fab0c1c658b9bbfdd975c685de60733d90250
Bruno Rijsman <brunorijsman@gmail.com>
2019-07-09 14:51:35 +0200

    Cleanup pre-commit-checks

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""Package tests."""
+
+import sys
+
+sys.path.append("quantum_path_computation_engine")
+sys.path.append("tests")
---
+++ tests/test_network_model.py
@@ -0,0 +1,8 @@
+"""Unit tests for module network_model."""
+
+import network_model
+
+def test_valid_network_file():
+    """Test reading a valid network YAML file."""
+    _model = network_model.read_network_model_from_file("tests/network-valid.yaml")
+    assert False
