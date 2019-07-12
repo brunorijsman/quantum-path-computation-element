836cbb8c34b87f7ba784616bfee67949c3970646
Bruno Rijsman <brunorijsman@gmail.com>
2019-07-12 22:19:02 +0200

    Break up into smaller functions

--- qpce/network_yaml.py
+++ qpce/network_yaml.py
@@ -35,6 +35,7 @@ NETWORK_SCHEMA = {
 }
 
 class ReadNetworkModelError(Exception):
+    # TODO: Rename this
     """Exception is thrown when there is a problem reading the network model."""
 
 class NetworkValidator(cerberus.Validator):
@@ -78,27 +79,59 @@ def read_network_from_yaml_stream(stream):
     if not validator.validate(network_model, NETWORK_SCHEMA):
         message = f"Could not validate network YAMLdocument"
         raise ReadNetworkModelError(message)
-        # TODO: Better error message
-        # pretty_printer = pprint.PrettyPrinter()
-        # pretty_printer.pprint(validator.errors)
+        # TODO: Better error message, reporting validator.errors using pretty printer
     network_model = validator.normalized(network_model)
+    return read_network_from_parsed_yaml(network_model)
+
+def read_network_from_parsed_yaml(network_model):
+    """Create a Network object from a network_model, i.e. from parsed YAML file.
+
+    Args:
+        network_model: The parsed network YAML file.
+    Returns:
+        A Network object.
+    Raises:
+        ReadNetworkModelError: There was a problem reading the network model.
+    """
     network = Network()
     if 'routers' in network_model:
         for router_model in network_model['routers']:
-            _router = Router(network=network, name=router_model['name'])
+            _router = read_router_from_parsed_yaml(router_model, network)
     if 'links' in network_model:
         for link_model in network_model['links']:
-            router_1_name = link_model['router-1']
-            if router_1_name in network.routers:
-                router_1 = network.routers[router_1_name]
-            else:
-                raise ReadNetworkModelError(f"Link has non-existent router-1 {router_1_name}")
-            router_2_name = link_model['router-2']
-            if router_2_name in network.routers:
-                router_2 = network.routers[router_2_name]
-            else:
-                raise ReadNetworkModelError(f"Link has non-existent router-2 {router_2_name}")
-            _link = Link(router_1=router_1,
-                         router_2=router_2,
-                         length=link_model['length'])
+            _link = read_link_from_parsed_yaml(link_model, network)
     return network
+
+def read_router_from_parsed_yaml(router_model, network):
+    """Create a Router object from a router_model, i.e. from parsed YAML file.
+
+    Args:
+        router_model: The parsed router YAML object.
+        network: Create the Router object in this network.
+    Returns:
+        The newly created Router object.
+    """
+    return Router(network=network, name=router_model['name'])
+
+def read_link_from_parsed_yaml(link_model, network):
+    """Create a Link object from a link_model, i.e. from parsed YAML file.
+
+    Args:
+        link_model: The parsed link YAML object.
+        network: Create the Link object in this network.
+    Returns:
+        The newly created Link object.
+    """
+    router_1_name = link_model['router-1']
+    if router_1_name in network.routers:
+        router_1 = network.routers[router_1_name]
+    else:
+        raise ReadNetworkModelError(f"Link has non-existent router-1 {router_1_name}")
+    router_2_name = link_model['router-2']
+    if router_2_name in network.routers:
+        router_2 = network.routers[router_2_name]
+    else:
+        raise ReadNetworkModelError(f"Link has non-existent router-2 {router_2_name}")
+    return Link(router_1=router_1,
+                router_2=router_2,
+                length=link_model['length'])
